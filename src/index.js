c8dbce0438f232924362d6b4684540d18ac84471
Adam Klein <adamklein@Adams-MacBook-Pro.local>
2019-03-12 17:05:41 +0200

    Rewrite - WIP

--- src/index.js
+++ src/index.js
@@ -1,20 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import { withHistory, ReuseProvider, createStore } from "./reuse";
-import { ReuseProvider } from "./reuse";
-import { Async } from "./Async";
+// import { ReuseProvider } from "./reuse";
+// import { Async } from "./Async";
 // import { TimeTravel } from "./TimeTravel";
 import "./styles.css";
+
+import {reuse, reuseState} from './reuse';
+
+const reuseCount = reuse(() => {
+  const [count, setCount] = reuseState(0);
+  const [step, setStep] = reuseState(0);
+
+  return {
+    count,
+    step,
+    setStep,
+    setCount
+  }
+});
+
+const reuseSelector = reuse(() => {
+  const {count} = reuseCount();
+
+  return count % 10;
+});
+
+
+const a1 = reuseCount();
+console.log('count, step', a1.count, a1.step);
+console.log('reuseSelector', reuseSelector());
+
+console.log('setCount(2)');
+reuseCount().setCount(2);
+
+const a2 = reuseCount();
+console.log('count, step', a2.count, a2.step);
+console.log('reuseSelector', reuseSelector());
+
+console.log('setStep(3)');
+reuseCount().setStep(3);
+
+const a3 = reuseCount();
+console.log('count, step', a3.count, a3.step);
+console.log('reuseSelector', reuseSelector());
 
 // const store = withHistory(createStore)();
 // const store = createStore();
 
 function App() {
-  return (
-    <ReuseProvider>
-      {/* <TimeTravel /> */}
-      <Async />
-    </ReuseProvider>
+  return (<div>bla</div>
   );
 }
 
--- src/reuse.js
+++ src/reuse.js
@@ -1,11 +1,29 @@
-import React, {
-  createContext,
-  useRef,
-  useState,
-  useContext,
-  useMemo,
-  useEffect
-} from "react";
+export const reuse = callback => {
+  units.set(callback, {hooks: []});
+
+  return () => {
+    // save
+    const prevUnitKey = currentUnitKey;
+    const prevHookIndex = currentHookIndex;
+
+    // reset
+    currentUnitKey = callback;
+    currentHookIndex = 0;
+
+    // call
+    const result = callback();
+
+    // restore
+    currentUnitKey = prevUnitKey;
+    currentHookIndex = prevHookIndex;
+
+    // return
+    return result;
+  }
+}
+// TBD: export const useReuse = unit => {
+
+// }
 
 const defaultReducer = (state, value) => {
   if (typeof value === "function") {
@@ -15,219 +33,39 @@ const defaultReducer = (state, value) => {
   }
 };
 
-// reuse
-export const createStore = () => {
-  let listeners = [];
-  let state = new Map();
-  let currentKey = null;
-  let currentHookIndex = 0;
-  const store = {
-    getCurrentHooks: () => {
-      if (!state.has(currentKey)) {
-        state.set(currentKey, []);
-      }
-      return state.get(currentKey);
-    },
-    setCurrentKey: (key) => {
-      currentKey = key;
-      currentHookIndex = 0;
-    },
-    useState: (initialValue, reducer = defaultReducer) => {
-      // get current hook
-      const hooks = store.getCurrentHooks();
-      const curIndex = currentHookIndex;
-      const key = currentKey;
-      // if it doesn't exist, create it according to initialValue
-      if (hooks.length <= currentHookIndex) {
-        // create tuple
-        const value =
-          typeof initialValue === "function" ? initialValue() : initialValue;
-        const setValue = newValue => {
-          hooks[curIndex][0] = reducer(hooks[curIndex][0], newValue);
-          store.notify(key);
-        };
+let currentUnitKey = null;
+let currentHookIndex = 0;
+let units = new Map();
 
-        // save it on the current index
-        hooks[curIndex] = [value, setValue];
-      }
-      // return current hook and advance index
-      return hooks[currentHookIndex++];
-    },
-    subscribe: (key, callbackFn) => {
-      // subscribe to specific key
-      const listener = { key, callbackFn };
-
-      listeners = [...listeners, listener];
-
-      return () => {
-        listeners = listeners.filter(item => item !== listener);
-      };
-    },
-    notify: key => {
-      // notify specific key
-      listeners.forEach(listener => {
-        if (key === listener.key) {
-          listener.callbackFn();
-        }
-      });
-    }
-    // replace: newState => {
-    //   state = newState;
-    //   listeners.forEach(({ key, callbackFn }) => {
-    //     callbackFn(get(key, state));
-    //   });
-    // }
-  };
-
-  window.store = store;
-
-  return store;
-};
-
-// reuse-react
-const ReuseContext = createContext();
-
-export const ReuseProvider = ({ store = null, children }) => {
-  const storeRef = useRef(store);
-
-  if (!storeRef.current) {
-    storeRef.current = createStore();
-  }
-
-  return (
-    <ReuseContext.Provider value={storeRef.current}>
-      {children}
-    </ReuseContext.Provider>
-  );
-};
-const mockStore = {
-  useState,
-  subscribe: () => {},
-  setCurrentKey: () => {}
-};
-
-export const reuse = param => {
-  let callback =
-    typeof param === "function" ? param : reuseState => reuseState(param);
-
-  return (subscribeCallback) => {
-    const store = useContext(ReuseContext) || mockStore;
-    let reuseState = store.useState;
-    store.setCurrentKey(callback);
-    const [value, setValue] = useState(() => callback(reuseState));
-    const update = () => setValue(callback(reuseState));
-
-    useEffect(() => store.subscribe(callback, subscribeCallback || update), []);
-
-    return value;
-  };
-};
-
-export const reuseMemo = (callback, reuseDeps) => {
-  return () => {
-    let onSubscribe;
-    const deps = reuseDeps.map(useSomething => useSomething(onSubscribe));
-    const [result, setResult] = useState(() => callback(deps));
-    onSubscribe = () => {
-      const deps = reuseDeps.map(useSomething => useSomething(onSubscribe));
-      const nextResult = callback(deps);
-      if (nextResult !== result) {
-        setResult(nextResult);
-      }
-
-    }
-    return useMemo(() => callback(deps), deps);
-  }
+export const reuseState = (initialState) => {
+  return reuseReducer(initialState, defaultReducer)
 }
 
-// export const reuseState = (path, initialValue, reducer = defaultReducer) => {
-//   const store = useContext(ReuseContext);
+export const reuseReducer = (initialState, reducer) => {
+  if (!currentUnitKey) {
+    throw new Error(`reuse hooks cannot be called outside of a reusable unit,
+e.g. const reuseCount = reuse(() => {
+  return reuseState(0);
+})')`);
+  }
 
-//   const [state, setState] = useState(() => {
-//     let result = get(path, store.getState());
+  const {hooks} = units.get(currentUnitKey);
 
-//     if (result === undefined) {
-//       if (initialValue === undefined) {
-//         throw new Error("Must supply initial value to reuseState");
-//       }
-//       result = initialValue;
-//     }
-//     return result;
-//   });
-//   useEffect(() => {
-//     return store.subscribe(path, value => setState(value));
-//   }, []);
-//   return [
-//     state,
-//     value => {
-//       const newState = reducer(state, value);
-//       store.update(path, newState);
-//     }
-//   ];
-// };
+  // If hook doesn't exist for this index, create it
+  if (hooks.length <= currentHookIndex) {
+    const curIndex = currentHookIndex; // For closure
+    const setState = (action) => {
+      const prevState = hooks[curIndex][0];
+      const newState = reducer(prevState, action);
 
-// time travel
-// export const withHistory = originalCreateStore => {
-//   return (initialState = {}) => {
-//     const store = originalCreateStore({
-//       ...initialState,
-//       __history: {
-//         items: [{}],
-//         index: 0
-//       }
-//     });
-//     const newStore = {
-//       ...store,
-//       update: (fullpath, value) => {
-//         store.update(fullpath, value);
-//         store.update("__history", __history => {
-//           // get items until current index
-//           const itemsTillNow = __history.items.slice(0, __history.index + 1);
-//           // save all state except time travel data
-//           const newItem = { ...store.getState() };
-//           delete newItem.__history;
+      hooks[curIndex][0] = newState;
+      // TBD: notify();
+    }
+    hooks[currentHookIndex] = [initialState, setState];
+  }
+  // Get current hook
+  let hook = hooks[currentHookIndex];
+  currentHookIndex++;
 
-//           // add to items and advance index
-//           const newItems = [...itemsTillNow, newItem];
-//           const newIndex = __history.index + 1;
-
-//           return {
-//             ...__history,
-//             items: newItems,
-//             index: newIndex
-//           };
-//         });
-//       }
-//     };
-
-//     return newStore;
-//   };
-// };
-
-// // react time travel
-// export const useTimeTravel = () => {
-//   const [timeTravel] = reuseState();
-//   const store = useContext(ReuseContext);
-
-//   const gotoIndex = index => {
-//     const newIndex = Math.min(Math.max(0, index), timeTravel.items.length - 1);
-//     const newState = {
-//       ...timeTravel.items[newIndex],
-//       __history: {
-//         ...timeTravel,
-//         index: newIndex
-//       }
-//     };
-//     store.replace(newState);
-//   };
-
-//   return {
-//     getItems: () => timeTravel.items,
-//     getIndex: () => timeTravel.index,
-//     canUndo: () => timeTravel.index > 0,
-//     canRedo: () => timeTravel.index < timeTravel.items.length - 1,
-//     undo: () => gotoIndex(timeTravel.index - 1),
-//     redo: () => gotoIndex(timeTravel.index + 1),
-//     gotoIndex
-//   };
-// };
+  return hook;
+}
