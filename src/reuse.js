6894b7117cb79161d5a20f8adbb32c77086ed306
Adam Klein <adam@500tech.com>
2019-03-19 13:12:30 +0200

    Update withReuse API

--- src/reuse.js
+++ src/reuse.js
@@ -180,7 +180,7 @@ const shallowCompare = (obj1, obj2) => {
 }
 
 // HOC
-export const withReuse = (unit, mapStateToProps) => (Comp) => {
+export const withReuse = (mapStateToProps, units) => (Comp) => {
   class WrappedComponent extends React.Component {
     state = {
       mappedProps: {}
@@ -188,22 +188,22 @@ export const withReuse = (unit, mapStateToProps) => (Comp) => {
     componentDidMount() {
       const store = this.context;
       setCurrentStore(store);
-      this.unsubscribe = store.subscribe(unit, () => {
+      this.unsubscribes = units.map(unit => store.subscribe(unit, () => {
         this.updateStateToProps();
-      })
+      }));
       this.updateStateToProps();
     }
+    componentWillUnmount() {
+      this.unsubscribes.forEach(unsubscribe => unsubscribe());
+    }
     updateStateToProps() {
-      const newState = reuse(unit);
-      const newProps = mapStateToProps(newState);
+      const newStates = units.map(unit => reuse(unit));
+      const newProps = mapStateToProps(newStates);
       const oldProps = this.state.mappedProps;
 
       if (!shallowCompare(oldProps, newProps)) {
         this.setState({ mappedProps: newProps });
       }
-    }
-    componentWillUnmount() {
-      this.unsubscribe();
     }
     render() { 
       const {forwardedRef, ...ownProps} = this.props;
