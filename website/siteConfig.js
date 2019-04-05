f7c338faaf6615b536bbb4f54fd62c60c849c304
Adam Klein <adam@500tech.com>
2019-04-05 19:11:30 +0300

    Update config for publish

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://reusable.github.io', // Your website URL
-  baseUrl: '/reusable/', // Base URL for your project */
+  baseUrl: '/reusable', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'reusable',
-  organizationName: 'reusablesjs',
+  organizationName: 'reusablejs',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
